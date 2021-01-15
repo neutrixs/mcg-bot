dab4133d2062c223966e3c9cf140935d34cbb474
Ihsan Rakhman <neutrixs@users.noreply.github.com>
2021-01-15 08:36:36 +0700

    update help ann

--- src/help/helplist/help ann.js
+++ src/help/helplist/help ann.js
@@ -14,7 +14,13 @@ module.exports = {
             '`-P "your text"`: Add a plain text followed by double quote\n'+
             '`-I imagelink`: Add an image inside embed\n'+
             '`--color #696969`: Set embed color with hex\n'+
-            '`--member`: Tag ETS2MCG Member role'
+            '`--member`: Tag ETS2MCG Member role',
+            false
+        )
+        .addField(
+            'Inserting Hyperlink',
+            'Format: `[text](https://example.com)`',
+            false
         )
         .setFooter(
             `Permission required:\nMANAGE_MESSAGES`
