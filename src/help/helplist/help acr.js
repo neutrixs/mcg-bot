6b4d79c2e17eaa8470719d95864917076ae7241e
Ihsan Rakhman <neutrixs@users.noreply.github.com>
2021-03-02 11:28:53 +0700

    more info and example

--- src/help/helplist/help acr.js
+++ src/help/helplist/help acr.js
@@ -6,6 +6,15 @@ module.exports = {
             `**Format:** \`${config.PREFIX}acr "commandname" "response" [embed]\`\n`+
             `[] = Optional`
         )
+        .addField(
+            'Wanna add embed?',
+            'see [this guide](https://discordjs.guide/popular-topics/embeds.html#using-an-embed-object) for more info',
+            false
+        )
+        .addField(
+            'Example',
+            `\`${config.PREFIX}acr ".banana" ":banana:" {"description":"banana","color":16776960}\``
+        )
         .setFooter(
             `Permission required:\nMANAGE_MESSAGES`
         )
