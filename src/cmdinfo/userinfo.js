eef3a2400126cb418453d72b8df44866f3053efa
Ihsan Rakhman <66576783+neutrixs@users.noreply.github.com>
2021-04-01 07:07:51 +0700

    Update userinfo.js
    fix error when trying to get user not in bot cache

--- src/cmdinfo/userinfo.js
+++ src/cmdinfo/userinfo.js
@@ -5,63 +5,70 @@ module.exports = {
         var embed = varstore.embed
         var requestedBy = `Requested by ${msg.author.username}#${msg.author.discriminator}`
 
-        if(args[1]){
-            args[1] = args[1].replace('<@','').replace('!','').replace('>','')
-            if(isNaN(args[1])){
-                embed = embed
-                .setDescription('User ID must be a number!(or tag the person if you don\'t know how)')
-                .setColor('#FF0000')
-                msg.channel.send(embed)
-                return
+
+        async function getnsend(){
+            if(args[1]){
+                args[1] = args[1].replace('<@','').replace('!','').replace('>','')
+                if(isNaN(args[1])){
+                    embed = embed
+                    .setDescription('User ID must be a number!(or tag the person if you don\'t know how)')
+                    .setColor('#FF0000')
+                    msg.channel.send(embed)
+                    return
+                }
+                msg.author = bot.users.cache.find(m=>m.id==args[1])
+                if(msg.author == undefined){
+                    msg.author = await bot.users.fetch(args[1])
+                }
             }
-            msg.author = bot.users.cache.find(m=>m.id==args[1])
-        }
 
-        //activity
-        useractivity = 'None';
-        if(msg.author.presence.activities.toString() !== ''){
-            if(msg.author.presence.activities[0].name == 'Spotify'){
-                spotify = msg.author.presence.activities[0];
-                useractivity = `Spotify:\n${spotify.details} by ${spotify.state}`
+            //activity
+            useractivity = 'None';
+            if(msg.author.presence.activities.toString() !== ''){
+                if(msg.author.presence.activities[0].name == 'Spotify'){
+                    spotify = msg.author.presence.activities[0];
+                    useractivity = `Spotify:\n${spotify.details} by ${spotify.state}`
+                }
+                else{
+                    presence = msg.author.presence.activities[0];
+                    useractivity = `${presence.type.toLowerCase()} ${presence.name}`
+                }
             }
-            else{
-                presence = msg.author.presence.activities[0];
-                useractivity = `${presence.type.toLowerCase()} ${presence.name}`
+
+            //joined discord
+            dateformat = {
+                day:'2-digit',
+                month:'short',
+                year:'numeric',
+                hour:'2-digit',
+                minute:'2-digit',
+                second:'2-digit',
+                timeZone:'Asia/Jakarta'
             }
+            joinedDiscord = msg.author.createdAt.toLocaleString('en-GB',dateformat)
+            joinedDiscord = joinedDiscord+' WIB'
+
+            //avatar
+            useravatar = msg.author.avatarURL().toString()
+            useravatar = useravatar.replace('.webp','?size=4096')
+
+            embed = embed
+            .setColor('#00FF00')
+            .setAuthor('USER INFO')
+            .setTitle(`Username: ${msg.author.username}#${msg.author.discriminator}`)
+            .addField('User ID',msg.author.id,true)
+            .addField('isBOT',msg.author.bot,true)
+            .addField('Status',msg.author.presence.status,true)
+            .addField('Joined Discord',joinedDiscord,true)
+            .addField('Activity',useractivity,true)
+
+            .addField('Avatar URL',useravatar,false)
+            .setThumbnail(useravatar)
+
+            .setFooter(requestedBy)
+            .setTimestamp()
+            msg.channel.send(embed)
         }
-
-        //joined discord
-        dateformat = {
-            day:'2-digit',
-            month:'short',
-            year:'numeric',
-            hour:'2-digit',
-            minute:'2-digit',
-            second:'2-digit',
-            timeZone:'Asia/Jakarta'
-        }
-        joinedDiscord = msg.author.createdAt.toLocaleString('en-GB',dateformat)
-        joinedDiscord = joinedDiscord+' WIB'
-
-        //avatar
-        useravatar = msg.author.avatarURL().toString()
-        useravatar = useravatar.replace('.webp','?size=4096')
-
-        embed = embed
-        .setColor('#00FF00')
-        .setAuthor('USER INFO')
-        .setTitle(`Username: ${msg.author.username}#${msg.author.discriminator}`)
-        .addField('User ID',msg.author.id,true)
-        .addField('isBOT',msg.author.bot,true)
-        .addField('Status',msg.author.presence.status,true)
-        .addField('Joined Discord',joinedDiscord,true)
-        .addField('Activity',useractivity,true)
-
-        .addField('Avatar URL',useravatar,false)
-        .setThumbnail(useravatar)
-
-        .setFooter(requestedBy)
-        .setTimestamp()
-        msg.channel.send(embed)
+        getnsend()
     }
 }
