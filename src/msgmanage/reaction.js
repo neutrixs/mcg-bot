388d1551e3458734623eab7c63b69467f2b5bd41
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-08-11 20:14:16 +0700

    prepare

--- src/msgmanage/reaction.js
+++ src/msgmanage/reaction.js
@@ -7,35 +7,34 @@ module.exports = {
         if(!msg.member.permissions.has(Permissions.FLAGS.MANAGE_MESSAGES)){
             msg.channel.send({embeds:[varstore.embednopermission]})
         }
-        
+
         if(!args[1]){
             embed = embed
             .setDescription(`Please specify something! see \`${config.PREFIX}help react\` for more info.`)
             .setColor('#FF0000')
-            msg.channel.send(embed)
+            msg.channel.send({embeds:[embed]})
             return
         }
         else if(args[1] && !args[2]){
             embed = embed
             .setDescription(`Please specify an emoji! see \`${config.PREFIX}help react\` for more info.`)
             .setColor('#FF0000')
-            msg.channel.send(embed)
+            msg.channel.send({embeds:[embed]})
             return
         }
         else if(isNaN(args[1]) == true){
             embed = embed
             .setDescription(`Message ID must be a number!`)
             .setColor('#FF0000')
-            msg.channel.send(embed)
+            msg.channel.send({embeds:[embed]})
             return
         }
         args[2] = args[2].replace(/[\\]/g,'')
         msg.channel.messages.fetch(args[1]).then(msg=>{
             msg.react(args[2]).catch(e=>{
-                msg.channel.send(
-                    varstore.embederror
+                embed = varstore.embederror
                     .setDescription(`\`\`\`${e}\`\`\``)
-                )
+                msg.channel.send({embeds:[embed]})
             })
         })
         msg.delete()
