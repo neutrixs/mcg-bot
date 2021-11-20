2ca2e5a1005a5379c90b23de6cffc60c0efcd52a
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-11-20 19:57:35 +0700

    move msgcreate handler

--- src/handler.ts
+++ src/handler.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js"
+import { Client, Message } from "discord.js"
 import { commandsType } from "./types/handlerTypes"
 
 export default class Handler {
@@ -11,12 +11,14 @@ export default class Handler {
             console.log(`Logged in as ${readyData.user.username}#${readyData.user.discriminator}`)
         })
 
-        client.on('messageCreate',msg=>{
-            console.log(msg.content)
-        })
+        client.on('messageCreate',this.msgCreateHandler)
     }
 
     public addCommands(commandsList:commandsType){
         this.commands = [...this.commands, ...commandsList]
+    }
+
+    private msgCreateHandler(msg:Message){
+        console.log(msg.content)
     }
 }
