67ad0378cb3ee2c81c5ba72c5b3af085e2486ac8
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2021-11-20 21:10:37 +0700

    set prefix function �

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,14 @@
 import { Client, Message } from "discord.js"
 import { commandsType } from "./types/handlerTypes"
+import config from '../config.js'
 
 export default class Handler {
     private commands:commandsType
+    private prefix:string
 
     public constructor(client: Client){
         this.commands = []
+        this.prefix = config.prefix
 
         client.on('ready',readyData=>{
             console.log(`Logged in as ${readyData.user.username}#${readyData.user.discriminator}`)
@@ -16,6 +19,10 @@ export default class Handler {
 
     public addCommands(commandsList:commandsType){
         this.commands = [...this.commands, ...commandsList]
+    }
+
+    public setPrefix(prefix:string){
+        this.prefix = prefix
     }
 
     private msgCreateHandler(msg:Message){
